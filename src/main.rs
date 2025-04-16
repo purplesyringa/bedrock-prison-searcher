f9174dde41a742c7713345faa178c3b864e11806
Alisa Sireneva <me@purplesyringa.moe>
2025-04-16 11:24:08 +0300

    Optimize preemptive scan with AVX-512

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+#![feature(portable_simd)]
+
 use core::fmt;
+use std::simd::{cmp::SimdPartialOrd, i32x8, i64x8, mask64x8, num::SimdInt, u64x8};
 use std::time::Instant;
 
 struct Xoroshiro {
@@ -65,27 +68,47 @@ impl BedrockFloorNoise {
     }
 
     fn is_bedrock(&self, x: i32, y: i32, z: i32) -> bool {
-        if y <= -64 {
-            true
-        } else if y >= -59 {
-            false
-        } else {
-            let block_key = hash_three(x, y, z);
-            let mut block_prng =
-                Xoroshiro::new(block_key ^ self.floor_seed_low, self.floor_seed_high);
+        assert!((-63..=-60).contains(&y));
 
-            // equivalent to
-            // let boundary = map_via_lerp(y as f64, -64.0, -59.0, 1.0, 0.0);
-            // (block_prng.next() >> 40) as f64 * 2.0f64.powi(-24) < boundary
+        let block_key = hash_three(x, y, z);
+        let mut block_prng = Xoroshiro::new(block_key ^ self.floor_seed_low, self.floor_seed_high);
 
-            static BOUNDARIES: [u64; 4] =
-                [13421773 << 40, 10066330 << 40, 6710887 << 40, 3355444 << 40];
-            block_prng.next() < BOUNDARIES[(y + 63) as usize]
-        }
+        // equivalent to
+        // let boundary = map_via_lerp(y as f64, -64.0, -59.0, 1.0, 0.0);
+        // (block_prng.next() >> 40) as f64 * 2.0f64.powi(-24) < boundary
+
+        static BOUNDARIES: [u64; 4] =
+            [13421773 << 40, 10066330 << 40, 6710887 << 40, 3355444 << 40];
+        block_prng.next() < BOUNDARIES[(y + 63) as usize]
+    }
+
+    fn is_bedrock_vec(&self, x: i32x8, y: i32, z: i32x8) -> mask64x8 {
+        assert!((-63..=-60).contains(&y));
+
+        let block_key = hash_three_vec(x, y, z);
+
+        let low = block_key ^ u64x8::splat(self.floor_seed_low);
+        let high = u64x8::splat(self.floor_seed_high);
+
+        assert_ne!(
+            self.floor_seed_high, 0,
+            "Cannot handle this rare case soundly"
+        );
+
+        let sum = low + high;
+        let block_prng_next = ((sum << 17) | (sum >> 47)) + low;
+
+        static BOUNDARIES: [u64; 4] =
+            [13421773 << 40, 10066330 << 40, 6710887 << 40, 3355444 << 40];
+        block_prng_next.simd_lt(u64x8::splat(BOUNDARIES[(y + 63) as usize]))
     }
 
     fn is_interior(&self, (x, z): (i32, i32)) -> bool {
         !self.is_bedrock(x, -63, z) && !self.is_bedrock(x, -62, z)
+    }
+
+    fn is_interior_vec(&self, x: i32x8, z: i32x8) -> mask64x8 {
+        !self.is_bedrock_vec(x, -63, z) & !self.is_bedrock_vec(x, -62, z)
     }
 
     fn get_column_type(&self, (x, z): (i32, i32)) -> ColumnType {
@@ -132,29 +155,75 @@ impl fmt::Display for ColumnType {
 // }
 
 fn hash_three(x: i32, y: i32, z: i32) -> u64 {
-    let mut l: i64 = (x.wrapping_mul(3129871) as i64) ^ (y as i64) ^ ((z as i64) * 116129781);
+    let mut l = (x.wrapping_mul(3129871) as i64) ^ (y as i64) ^ ((z as i64) * 116129781);
     l = l.wrapping_mul(l.wrapping_mul(42317861).wrapping_add(11));
     (l >> 16) as u64
+}
+
+fn hash_three_vec(x: i32x8, y: i32, z: i32x8) -> u64x8 {
+    let mut l = (x * i32x8::splat(3129871)).cast::<i64>()
+        ^ i64x8::splat(y as i64)
+        ^ (z.cast::<i64>() * i64x8::splat(116129781));
+    l = l * (l * i64x8::splat(42317861) + i64x8::splat(11));
+    (l >> 16).cast::<u64>()
 }
 
 const WORLD_BORDER: i32 = 29_999_984;
 const SEARCH_RADIUS: i32 = 4;
 
-fn enumerate_diagonals(mut callback: impl FnMut((i32, i32))) {
-    for i in (-2 * WORLD_BORDER..=2 * WORLD_BORDER).step_by(2 * SEARCH_RADIUS as usize).take(10000) {
+fn enumerate_diagonals(noise: &BedrockFloorNoise, mut callback: impl FnMut((i32, i32))) {
+    for i in (-2 * WORLD_BORDER..=2 * WORLD_BORDER)
+        .step_by(2 * SEARCH_RADIUS as usize)
+        .take(10000)
+    {
         // Main diagonals
-        let j_min = -WORLD_BORDER - i.min(0);
+        let mut j_min = -WORLD_BORDER - i.min(0);
         let j_max = WORLD_BORDER - i.max(0);
-        for j in j_min..=j_max {
-            callback((j, i + j));
+        while j_min + 7 <= j_max {
+            let x = i32x8::splat(j_min) + i32x8::from([0, 1, 2, 3, 4, 5, 6, 7]);
+            let z = i32x8::splat(i) + x;
+            let mut bitmask = noise.is_interior_vec(x, z).to_bitmask();
+            while bitmask != 0 {
+                let dj = bitmask.trailing_zeros() as i32;
+                callback((j_min + dj, i + j_min + dj));
+                bitmask &= bitmask - 1;
+            }
+            j_min += 8;
+        }
+        while j_min <= j_max {
+            let coords0 = (j_min, i + j_min);
+            if noise.is_interior(coords0) {
+                callback(coords0);
+            }
+            j_min += 1;
         }
 
         // Anti-diagonals
         let j_min = -WORLD_BORDER + i.max(0);
         let j_max = WORLD_BORDER + i.min(0);
-        for j in j_min..=j_max {
-            if j % SEARCH_RADIUS != 0 {
-                callback((j, i - j));
+        for j_rem in 0..SEARCH_RADIUS {
+            if (j_min + j_rem) % SEARCH_RADIUS == 0 {
+                continue;
+            }
+            let mut j_min = j_min + j_rem;
+            while j_min + 7 * SEARCH_RADIUS <= j_max {
+                let x = i32x8::splat(j_min)
+                    + i32x8::from([0, 1, 2, 3, 4, 5, 6, 7].map(|dj| dj * SEARCH_RADIUS));
+                let z = i32x8::splat(i) - x;
+                let mut bitmask = noise.is_interior_vec(x, z).to_bitmask();
+                while bitmask != 0 {
+                    let dj = bitmask.trailing_zeros() as i32 * SEARCH_RADIUS;
+                    callback((j_min + dj, i - j_min - dj));
+                    bitmask &= bitmask - 1;
+                }
+                j_min += 8 * SEARCH_RADIUS;
+            }
+            while j_min <= j_max {
+                let coords0 = (j_min, i - j_min);
+                if noise.is_interior(coords0) {
+                    callback(coords0);
+                }
+                j_min += SEARCH_RADIUS;
             }
         }
     }
@@ -195,12 +264,9 @@ fn main() {
     let mut queue = [(0, 0); 32 * 32];
 
     enumerate_diagonals(
+        &noise,
         #[inline(always)]
         |coords0| {
-            if !noise.is_interior(coords0) {
-                return;
-            }
-
             let (x0, z0) = coords0;
 
             let mut visited = CoordSet::new();
